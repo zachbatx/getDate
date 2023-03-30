55fd9d1f2b8e70c6407edfc517b6b4960acfa847
zachbatx <79647546+zachbatx@users.noreply.github.com>
2023-03-30 07:15:18 -0500

    Add files via upload

--- bookmk4.js
+++ bookmk4.js
@@ -1,43 +1,75 @@
 javascript:(function() {
-  let focusableElems = document.querySelectorAll('a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])');
-  let headingElems = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+	
+window.onload = startBkMk();
+function startBkMk() {
+  const toggleButtonId = "toggle-focusable-elements";
+  const focusableElementsClass = "focusable-element-badge";
+
+  function createToggle() {
+    const button = document.createElement("button");
+    button.id = toggleButtonId;
+    button.innerText = "Toggle Focusable Elements";
+    button.style.position = "fixed";
+    button.style.top = "10px";
+    button.style.right = "10px";
+    button.style.zIndex = "10000";
+    document.body.appendChild(button);
+    return button;
+  }
 
   function createBadge(content) {
-    const badge = document.createElement('span');
+    const badge = document.createElement("span");
     badge.innerText = content;
-    badge.style.position = 'absolute';
-    badge.style.backgroundColor = 'red';
-    badge.style.color = 'white';
-    badge.style.padding = '1px 4px';
-    badge.style.fontSize = '12px';
-    badge.style.borderRadius = '3px';
-    badge.style.zIndex = '9999';
+    badge.classList.add(focusableElementsClass);
+    badge.style.position = "absolute";
+    badge.style.backgroundColor = "red";
+    badge.style.color = "white";
+    badge.style.padding = "1px 4px";
+    badge.style.fontSize = "12px";
+    badge.style.borderRadius = "3px";
+    badge.style.zIndex = "9999";
     return badge;
   }
 
   function addBadgeToElem(elem, badge) {
     const rect = elem.getBoundingClientRect();
-    badge.style.top = (rect.top + window.scrollY - badge.offsetHeight / 2) + 'px';
-    badge.style.left = (rect.left + window.scrollX - badge.offsetWidth / 2) + 'px';
+    badge.style.top = (rect.top + window.scrollY - badge.offsetHeight / 2) + "px";
+    badge.style.left = (rect.left + window.scrollX - badge.offsetWidth / 2) + "px";
     document.body.appendChild(badge);
   }
 
-  focusableElems.forEach((elem, index) => {
-    elem.style.outline = '3px solid yellow';
-    elem.addEventListener('focus', () => {
-      elem.style.outline = '3px solid blue';
-    });
-    elem.addEventListener('blur', () => {
-      elem.style.outline = '3px solid yellow';
-    });
+  function highlightFocusableElements() {
+    const focusableElems = document.querySelectorAll(
+      'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"]):not([id="' + toggleButtonId + '"])'
+    );
 
-    const badge = createBadge(index + 1);
-    addBadgeToElem(elem, badge);
-  });
+    focusableElems.forEach((elem, index) => {
+      const badge = createBadge(index + 1);
+      addBadgeToElem(elem, badge);
+    });
+  }
 
-  headingElems.forEach((elem) => {
-    const headingLevel = elem.tagName.match(/\d+/)[0];
-    const badge = createBadge('H' + headingLevel);
-    addBadgeToElem(elem, badge);
-  });
+  function removeHighlightFocusableElements() {
+    const badges = document.querySelectorAll("." + focusableElementsClass);
+    badges.forEach((badge) => {
+      badge.remove();
+    });
+  }
+
+  function toggleFocusableElements() {
+    const badges = document.querySelectorAll("." + focusableElementsClass);
+    if (badges.length === 0) {
+      highlightFocusableElements();
+    } else {
+      removeHighlightFocusableElements();
+    }
+  }
+
+  const existingToggleButton = document.getElementById(toggleButtonId);
+  if (!existingToggleButton) {
+    const toggleButton = createToggle();
+    toggleButton.addEventListener("click", toggleFocusableElements);
+  }
+}
+
 })();
